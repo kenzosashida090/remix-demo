408e5dfa186a4193493cd459fcc060b36aa33538
kenzosashida090 <kenzosashida1@gmail.com>
2024-10-03 22:38:46 -0600

    [new] Adding index.tsx page

---
+++ app/routes/_index.tsx
@@ -0,0 +1,10 @@
+export default function Index(){
+    return(
+	<p id='index-page'>
+	    This is a demo for Remix.
+	    <br/>
+	    Check Out
+	    <a href="https://remix.run">The docs at remix.run</a>
+	</p>
+    )
+}
